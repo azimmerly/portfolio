9d220026d3ccf25872be8898ec9a67a48e99e675
Andrew Zimmerly <83731749+azimmerly@users.noreply.github.com>
2025-03-10 10:18:25 -0700

    Update dependencies

---
+++ prettier.config.mjs
@@ -0,0 +1,6 @@
+/* eslint-disable import/no-anonymous-default-export */
+/** @type {import("prettier").Config} */
+
+export default {
+  plugins: ["prettier-plugin-organize-imports", "prettier-plugin-tailwindcss"],
+};
