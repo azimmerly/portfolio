0e10d4eb545ee78d0d4563aeab3a34afe2859124
Andrew Zimmerly <83731749+azimmerly@users.noreply.github.com>
2025-10-29 07:41:10 -0700

    Add vercel analytics

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,9 +1,11 @@
+import { Analytics } from "@vercel/analytics/next";
 import { ThemeProvider } from "next-themes";
 
 import { Toaster } from "@/components/Toaster";
 
 export const Providers = ({ children }: { children: React.ReactNode }) => (
   <ThemeProvider defaultTheme="system" enableSystem>
+    <Analytics />
     <Toaster />
     {children}
   </ThemeProvider>
